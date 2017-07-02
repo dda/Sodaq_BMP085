6a500394f1fde4ceaacb58e58e5240ebddc903ec
dda <headspin@gmail.com>
2017-07-02 13:54:23 +0800

    Update Sodaq_BMP085.cpp

--- src/Sodaq_BMP085.cpp
+++ src/Sodaq_BMP085.cpp
@@ -47,17 +47,11 @@ Sodaq_BMP085::Sodaq_BMP085()
   oversampling = BMP085_ULTRAHIGHRES;
 }
 
-void Sodaq_BMP085::begin(uint8_t mode)
-{
-  if (mode > BMP085_ULTRAHIGHRES)
-    mode = BMP085_ULTRAHIGHRES;
+void Sodaq_BMP085::begin(uint8_t mode) {
+  if (mode > BMP085_ULTRAHIGHRES) mode = BMP085_ULTRAHIGHRES;
   oversampling = mode;
-
   Wire.begin();
-
-  if (read8(0xD0) != 0x55)
-    return;
-
+  if (read8(0xD0) != 0x55) return;
   /* read calibration data */
   ac1 = read16(BMP085_CAL_AC1);
   ac2 = read16(BMP085_CAL_AC2);
@@ -65,10 +59,8 @@ void Sodaq_BMP085::begin(uint8_t mode)
   ac4 = read16(BMP085_CAL_AC4);
   ac5 = read16(BMP085_CAL_AC5);
   ac6 = read16(BMP085_CAL_AC6);
-
   b1 = read16(BMP085_CAL_B1);
   b2 = read16(BMP085_CAL_B2);
-
   mb = read16(BMP085_CAL_MB);
   mc = read16(BMP085_CAL_MC);
   md = read16(BMP085_CAL_MD);
@@ -79,18 +71,15 @@ void Sodaq_BMP085::begin(uint8_t mode)
   Serial.print("ac4 = "); Serial.println(ac4, DEC);
   Serial.print("ac5 = "); Serial.println(ac5, DEC);
   Serial.print("ac6 = "); Serial.println(ac6, DEC);
-
   Serial.print("b1 = "); Serial.println(b1, DEC);
   Serial.print("b2 = "); Serial.println(b2, DEC);
-
   Serial.print("mb = "); Serial.println(mb, DEC);
   Serial.print("mc = "); Serial.println(mc, DEC);
   Serial.print("md = "); Serial.println(md, DEC);
 #endif
 }
 
-uint16_t Sodaq_BMP085::readRawTemperature(void)
-{
+uint16_t Sodaq_BMP085::readRawTemperature(void) {
   if ((ac1 == 0 || ac1 == -1) && (mb == 0 || mb == -1)) {
     // The device parameters were not read properly, or begin() was never executed
     begin(oversampling);
@@ -103,27 +92,17 @@ uint16_t Sodaq_BMP085::readRawTemperature(void)
   return read16(BMP085_TEMPDATA);
 }
 
-uint32_t Sodaq_BMP085::readRawPressure(void)
-{
+uint32_t Sodaq_BMP085::readRawPressure(void) {
   uint32_t raw;
-
   write8(BMP085_CONTROL, BMP085_READPRESSURECMD + (oversampling << 6));
-
-  if (oversampling == BMP085_ULTRALOWPOWER)
-    delay(5);
-  else if (oversampling == BMP085_STANDARD)
-    delay(8);
-  else if (oversampling == BMP085_HIGHRES)
-    delay(14);
-  else
-    delay(26);
-
+  if (oversampling == BMP085_ULTRALOWPOWER) delay(5);
+  else if (oversampling == BMP085_STANDARD) delay(8);
+  else if (oversampling == BMP085_HIGHRES) delay(14);
+  else delay(26);
   raw = read16(BMP085_PRESSUREDATA);
-
   raw <<= 8;
   raw |= read8(BMP085_PRESSUREDATA + 2);
   raw >>= (8 - oversampling);
-
   /* this pull broke stuff, look at it later? */
 #if 0
   if (oversampling==0) {
@@ -132,7 +111,6 @@ uint32_t Sodaq_BMP085::readRawPressure(void)
     raw >>= (8 - oversampling);
   }
 #endif
-
 #if BMP085_ENABLE_DIAG
   Serial.print("Raw pressure: "); Serial.println(raw);
 #endif
@@ -147,15 +125,13 @@ uint32_t Sodaq_BMP085::readRawPressure(void)
  *  X2 = MC * 2^11 / (X1 + MD)
  *  B5 = X1 + X2
  */
-int32_t Sodaq_BMP085::computeB5(int32_t UT)
-{
+int32_t Sodaq_BMP085::computeB5(int32_t UT) {
   int32_t X1;
   int32_t X2;
   int32_t B5;
   X1 = ((UT - ac6) * ac5) >> 15;
   X2 = ((int32_t) mc << 11) / (X1 + md);
   B5 = X1 + X2;
-
 #if BMP085_ENABLE_DIAG
   Serial.print("X1 = "); Serial.println(X1);
   Serial.print("X2 = "); Serial.println(X2);
@@ -164,8 +140,7 @@ int32_t Sodaq_BMP085::computeB5(int32_t UT)
   return B5;
 }
 
-int32_t Sodaq_BMP085::readPressure(void)
-{
+int32_t Sodaq_BMP085::readPressure(void) {
   int32_t UT;
   int32_t UP;
   int32_t B3;
@@ -177,10 +152,8 @@ int32_t Sodaq_BMP085::readPressure(void)
   int32_t p;
   uint32_t B4;
   uint32_t B7;
-
   UT = readRawTemperature();
   UP = readRawPressure();
-
 #if BMP085_DEBUG == 1
   // use datasheet numbers!
   UT = 27898;
@@ -197,37 +170,31 @@ int32_t Sodaq_BMP085::readPressure(void)
   ac4 = 32741;
   oversampling = 0;
 #endif
-
   // do temperature calculations
   B5 = computeB5(UT);
-
   // do pressure calcs
   B6 = B5 - 4000;
   X1 = ((int32_t) b2 * ((B6 * B6) >> 12)) >> 11;
   X2 = ((int32_t) ac2 * B6) >> 11;
   X3 = X1 + X2;
   B3 = ((((int32_t) ac1 * 4 + X3) << oversampling) + 2) / 4;
-
 #if BMP085_ENABLE_DIAG
   Serial.print("B6 = "); Serial.println(B6);
   Serial.print("X1 = "); Serial.println(X1);
   Serial.print("X2 = "); Serial.println(X2);
   Serial.print("B3 = "); Serial.println(B3);
 #endif
-
   X1 = ((int32_t) ac3 * B6) >> 13;
   X2 = ((int32_t) b1 * ((B6 * B6) >> 12)) >> 16;
   X3 = ((X1 + X2) + 2) >> 2;
   B4 = ((uint32_t) ac4 * (uint32_t) (X3 + 32768)) >> 15;
   B7 = ((uint32_t) UP - B3) * (uint32_t) (50000UL >> oversampling);
-
 #if BMP085_ENABLE_DIAG
   Serial.print("X1 = "); Serial.println(X1);
   Serial.print("X2 = "); Serial.println(X2);
   Serial.print("B4 = "); Serial.println(B4);
   Serial.print("B7 = "); Serial.println(B7);
 #endif
-
   if (B7 < 0x80000000) {
     p = (B7 * 2) / B4;
   } else {
@@ -236,13 +203,11 @@ int32_t Sodaq_BMP085::readPressure(void)
   X1 = (p >> 8) * (p >> 8);
   X1 = (X1 * 3038) >> 16;
   X2 = (-7357 * p) >> 16;
-
 #if BMP085_ENABLE_DIAG
   Serial.print("p = "); Serial.println(p);
   Serial.print("X1 = "); Serial.println(X1);
   Serial.print("X2 = "); Serial.println(X2);
 #endif
-
   p = p + ((X1 + X2 + (int32_t) 3791) >> 4);
 #if BMP085_ENABLE_DIAG
   Serial.print("p = "); Serial.println(p);
@@ -260,13 +225,10 @@ int32_t Sodaq_BMP085::readPressure(void)
  *  T = (B5 + 8) / 2^4
  * and this is in units of 0.1 degrees Celcius
  */
-float Sodaq_BMP085::readTemperature(void)
-{
+float Sodaq_BMP085::readTemperature(void) {
   int32_t UT, B5;
   float temp;
-
   UT = readRawTemperature();
-
 #if BMP085_DEBUG == 1
   // use datasheet numbers!
   UT = 27898;
@@ -275,12 +237,10 @@ float Sodaq_BMP085::readTemperature(void)
   mc = -8711;
   md = 2868;
 #endif
-
   // step 1
   B5 = computeB5(UT);
   temp = (B5 + 8) >> 4;
   temp /= 10;
-
   return temp;
 }
 
@@ -288,75 +248,58 @@ float Sodaq_BMP085::readTemperature(void)
  * Read the pressure calculated to Sea Level Pressure based on the passed
  * altitude and local gravity
  */
-int32_t Sodaq_BMP085::readPressure(int32_t altitude, float gravity)
-{
+int32_t Sodaq_BMP085::readPressure(int32_t altitude, float gravity) {
   float Rd = 287.0f; // dry gas constant for the atmosphere
   int32_t pressure = readPressure();
   float temperature = readTemperature();
-  int32_t seaLevelPressure = pressure + pressure -
-          ((pressure * 1000) / exp((gravity * altitude) / (Rd * (temperature + 273.15)))) / 1000;
+  int32_t seaLevelPressure = pressure + pressure - ((pressure * 1000) / exp((gravity * altitude) / (Rd * (temperature + 273.15)))) / 1000;
   return seaLevelPressure;
 }
 
-float Sodaq_BMP085::readAltitude(float sealevelPressure)
-{
+float Sodaq_BMP085::readAltitude(float sealevelPressure) {
   float altitude;
-
   float pressure = readPressure();
-
   altitude = 44330 * (1.0 - pow(pressure / sealevelPressure, 0.1903));
-
   return altitude;
 }
 
-
 /*********************************************************************/
-
 /*
  * Read a 1 byte register
  */
-uint8_t Sodaq_BMP085::read8(uint8_t a)
-{
+uint8_t Sodaq_BMP085::read8(uint8_t a) {
   uint8_t ret;
-
   Wire.beginTransmission(BMP085_I2CADDR);       // start transmission to device
   Wire.write(a);                                // sends register address to read from
   Wire.endTransmission();                       // end transmission
-
   Wire.beginTransmission(BMP085_I2CADDR);       // start transmission to device
   Wire.requestFrom(BMP085_I2CADDR, 1);          // send data n-bytes read
   ret = Wire.read();
   Wire.endTransmission();                       // end transmission
-
   return ret;
 }
 
 /*
  * Read a 2 byte register
  */
-uint16_t Sodaq_BMP085::read16(uint8_t a)
-{
+uint16_t Sodaq_BMP085::read16(uint8_t a) {
   uint16_t ret;
-
   Wire.beginTransmission(BMP085_I2CADDR);
   Wire.write(a);
   Wire.endTransmission();
-
   Wire.beginTransmission(BMP085_I2CADDR);
   Wire.requestFrom(BMP085_I2CADDR, 2);
   ret = Wire.read();
   ret <<= 8;
   ret |= Wire.read();
   Wire.endTransmission();
-
   return ret;
 }
 
 /*
  * Write a 1 byte register
  */
-void Sodaq_BMP085::write8(uint8_t a, uint8_t d)
-{
+void Sodaq_BMP085::write8(uint8_t a, uint8_t d) {
   Wire.beginTransmission(BMP085_I2CADDR);
   Wire.write(a);
   Wire.write(d);
