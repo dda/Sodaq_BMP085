6f2da21cfc57b15f09a9e919fd550e286a88364d
dda <headspin@gmail.com>
2017-07-02 13:54:59 +0800

    Update Sodaq_BMP085.cpp

--- src/Sodaq_BMP085.cpp
+++ src/Sodaq_BMP085.cpp
@@ -1,16 +1,13 @@
 /*************************************************** 
   This is a library for the Adafruit BMP085/BMP180 Barometric Pressure + Temp sensor
-
   Designed specifically to work with the Adafruit BMP085 or BMP180 Breakout 
   ----> http://www.adafruit.com/products/391
   ----> http://www.adafruit.com/products/1603
-
   These displays use I2C to communicate, 2 pins are required to  
   interface
   Adafruit invests time and resources providing this open source code, 
   please support Adafruit and open-source hardware by purchasing 
   products from Adafruit!
-
   Written by Limor Fried/Ladyada for Adafruit Industries.  
   BSD license, all text above must be included in any redistribution
  ****************************************************/
@@ -21,9 +18,7 @@
 
 #define BMP085_DEBUG 0
 #define BMP085_ENABLE_DIAG 0
-
 #define BMP085_I2CADDR 0x77
-
 #define BMP085_CAL_AC1           0xAA  // R   Calibration data (16 bits)
 #define BMP085_CAL_AC2           0xAC  // R   Calibration data (16 bits)
 #define BMP085_CAL_AC3           0xAE  // R   Calibration data (16 bits)
@@ -35,15 +30,13 @@
 #define BMP085_CAL_MB            0xBA  // R   Calibration data (16 bits)
 #define BMP085_CAL_MC            0xBC  // R   Calibration data (16 bits)
 #define BMP085_CAL_MD            0xBE  // R   Calibration data (16 bits)
-
 #define BMP085_CONTROL           0xF4
 #define BMP085_TEMPDATA          0xF6
 #define BMP085_PRESSUREDATA      0xF6
 #define BMP085_READTEMPCMD       0x2E
 #define BMP085_READPRESSURECMD   0x34
 
-Sodaq_BMP085::Sodaq_BMP085()
-{
+Sodaq_BMP085::Sodaq_BMP085() {
   oversampling = BMP085_ULTRAHIGHRES;
 }
 
